0ba43e5ccd47cdbb8d5cc1ebb118e3f64e677998
Gaurav Goswami <gauravg3070@gmail.com>
2023-05-13 14:33:35 +0530

    push 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,14 +1,14 @@
 package com.driver;
 
 public class Main {
-    public static class A{
+     static class A{
         public String meth()
         {
             return "Invoking method from class A";
         }
     }
 }
-    public static class B extends A{
+     static class B extends A{
 
         public B(){
             A obj= new A();
